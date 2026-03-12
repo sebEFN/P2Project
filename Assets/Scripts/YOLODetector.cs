1bf0d20e9641236faad7b28e45a3fe8a088ec00c
sebEFN <94054113+sebEFN@users.noreply.github.com>
2026-03-12 16:17:44 +0100

    webcam

---
+++ Assets/Scripts/YOLODetector.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using Unity.InferenceEngine;
+
+public class YOLODetector : MonoBehaviour
+{
+    public ModelAsset modelAsset;
+    private Worker worker;
+    public Texture2D testTexture;
+
+    void Start()
+    {
+        var model = ModelLoader.Load(modelAsset);
+        worker = new Worker(model, BackendType.GPUCompute);
+    }
+
+    public void Run(Texture2D image)
+    {
+        Tensor input = TextureConverter.ToTensor(image, 640, 640, 3);
+
+        worker.Schedule(input);
+
+        Tensor output = worker.PeekOutput();
+
+        Debug.Log(output.shape);
+    }
+    void Update()
+    {
+        Run(testTexture);
+    }
+    void OnDestroy()
+    {
+        worker?.Dispose();
+    }
+}
