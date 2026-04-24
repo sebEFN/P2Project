d1a9a66423aa787d25ded2032c3cafc35c2d47d5
donson-7 <kingmomoh240@gmail.com>
2026-04-24 16:34:10 +0200

    Sound Works!
    EUREKA!! I implemented the sound for when the mirror appears and added that to when you tap the menu as a placeholder.

--- Assets/Scripts/Audio/FMODEvents.cs
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -6,7 +6,11 @@ public class FMODEvents : MonoBehaviour
     [field: Header("Ambience")]
     [field: SerializeField] public EventReference Ambience { get; private set; }
 
+    [field: Header("Menu Open")]
+    [field: SerializeField] public EventReference MenuOpen { get; private set; }
+    
     public static FMODEvents Instance { get; private set; }
+    
     private void Awake()
     {
         if (Instance != null)
