cf917bbe2d48b4aa9e6d397f7db487ae83efc7cc
sebEFN <94054113+sebEFN@users.noreply.github.com>
2026-05-04 12:50:08 +0200

    chest

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    [SerializeField] Signs giveSign;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
